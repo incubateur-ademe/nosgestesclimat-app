f90da09b4f325149cfa618e2d4c2ad17ed0146a7
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2026-04-07 12:18:46 +0200

    add E2E test

--- e2e/pages/iframe.spec.ts
+++ e2e/pages/iframe.spec.ts
@@ -1,5 +1,59 @@
 import { expect, test } from '@playwright/test'
 
+test.describe('/demo-iframe-datashare.html', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/demos/demo-iframe-datashare.html')
+  })
+
+  test('displays the data-share modal when simulation is complete', async ({
+    page,
+  }) => {
+    const iframe = page.frameLocator('iframe').first()
+
+    // await expect(iframe.getByTestId('do-the-test-link')).toBeVisible({
+    //   timeout: 5000,
+    // })
+    await iframe.getByTestId('do-the-test-link').first().click({
+      timeout: 3000,
+    })
+
+    await expect(iframe.getByTestId('skip-tutorial-button')).toBeVisible()
+    await iframe.getByTestId('skip-tutorial-button').click()
+
+    const ngcTest = iframe.locator('body')
+
+    await expect(ngcTest.getByTestId('skip-question-button')).toBeVisible()
+
+    while (
+      !(await ngcTest
+        .getByTestId('divers . tabac . consommation par semaine')
+        .isVisible())
+    ) {
+      const skipButton = iframe.getByTestId('skip-question-button')
+      if (await skipButton.isVisible()) {
+        await skipButton.click()
+      } else {
+        break
+      }
+    }
+
+    const endTestButton = iframe.getByTestId('end-test-button')
+    if (await endTestButton.isVisible()) {
+      await endTestButton.click()
+    }
+
+    await expect(
+      iframe.locator('[data-testid="iframe-datashare-modal"]')
+    ).toBeVisible({
+      timeout: 10000,
+    })
+
+    await expect(iframe.getByTestId('iframe-datashare-title')).toBeVisible()
+    await expect(iframe.getByTestId('iframe-datashare-accepter')).toBeVisible()
+    await expect(iframe.getByTestId('iframe-datashare-refuser')).toBeVisible()
+  })
+})
+
 test.describe('/demo-iframe.html', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('/demo-iframe.html')
