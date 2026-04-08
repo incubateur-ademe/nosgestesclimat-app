a6b7f8cd2790b01355be775f0577862d0d2554fe
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2026-04-08 18:05:50 +0200

    🎨 Améliorations

--- src/app/[locale]/(simulation)/tutoriel/jeune/_components/YouthTutorial.tsx
+++ src/app/[locale]/(simulation)/tutoriel/jeune/_components/YouthTutorial.tsx
@@ -11,10 +11,34 @@ interface Props {
   locale: Locale
 }
 
+const MiniCard = ({
+  emoji,
+  title,
+  subtitle,
+}: {
+  emoji: string
+  title: React.ReactNode
+  subtitle: React.ReactNode
+}) => {
+  return (
+    <Card
+      tag="li"
+      className="shadow-primary-100 col-span-1 max-w-full flex-col justify-center gap-1 rounded-2xl border-0 text-center text-sm shadow-lg drop-shadow-xs md:w-56 md:text-base">
+      <Emoji className="text-3xl">{emoji}</Emoji>
+
+      <p className="mb-0">
+        <strong>{title}</strong>
+      </p>
+
+      <p>{subtitle}</p>
+    </Card>
+  )
+}
+
 // Version displayed for pupils and students
 export default function YouthTutorial({ locale }: Props) {
   return (
-    <div className="flex flex-col">
+    <div className="mb-16 flex flex-col">
       <section className="order-0 mt-8 mb-8 w-xl max-w-full md:mx-auto md:mb-10">
         <div className="mb-2 flex items-center md:gap-4">
           <Title hasSeparator={false} size="lg">
@@ -34,7 +58,7 @@ export default function YouthTutorial({ locale }: Props) {
           </div>
         </div>
 
-        <p className="mb-6 text-center md:text-left">
+        <p className="mb-6 text-center">
           <Trans locale={locale} i18nKey="youthTutorial.subtitle">
             Un test rapide pour comprendre ton empreinte carbone et agir !
           </Trans>
@@ -46,7 +70,7 @@ export default function YouthTutorial({ locale }: Props) {
               <Emoji className="mr-1">⏱️</Emoji>
 
               <Trans locale={locale} i18nKey="youthTutorial.list.time">
-                5 minutes
+                Rapide
               </Trans>
             </div>
           </li>
@@ -74,99 +98,75 @@ export default function YouthTutorial({ locale }: Props) {
       </section>
 
       <ul className="order-1 mb-12 grid grid-cols-2 gap-2 md:order-0 md:flex md:gap-8">
-        <Card
-          tag="li"
-          className="shadow-primary-100 col-span-1 max-w-full flex-col justify-center rounded-2xl border-0 text-center text-sm shadow-lg drop-shadow-xs md:w-56 md:text-base">
-          <Emoji className="text-3xl">🙋‍♀️</Emoji>
-
-          <p>
-            <strong>
-              <Trans
-                locale={locale}
-                i18nKey="youthTutorial.secondlist.forYou.title">
-                Réponds pour toi
-              </Trans>
-            </strong>
-          </p>
-
-          <p>
+        <MiniCard
+          emoji={'🙋‍♀️'}
+          title={
+            <Trans
+              locale={locale}
+              i18nKey="youthTutorial.secondlist.forYou.title">
+              Réponds pour toi
+            </Trans>
+          }
+          subtitle={
             <Trans
               locale={locale}
               i18nKey="youthTutorial.secondlist.forYou.body">
               C'est ton empreinte, pas celle de ta famille
             </Trans>
-          </p>
-        </Card>
+          }
+        />
 
-        <Card
-          tag="li"
-          className="shadow-primary-100 col-span-1 max-w-full flex-col justify-center rounded-2xl border-0 text-center text-sm shadow-lg drop-shadow-xs md:w-56 md:text-base">
-          <Emoji className="text-3xl">🤷‍♂️</Emoji>
-
-          <p>
-            <strong>
-              <Trans
-                locale={locale}
-                i18nKey="youthTutorial.secondlist.dontKnow.title">
-                Pas grave si tu sais pas
-              </Trans>
-            </strong>
-          </p>
-
-          <p>
+        <MiniCard
+          emoji={'🤷‍♂️'}
+          title={
+            <Trans
+              locale={locale}
+              i18nKey="youthTutorial.secondlist.dontKnow.title">
+              Pas grave si tu sais pas
+            </Trans>
+          }
+          subtitle={
             <Trans
               locale={locale}
               i18nKey="youthTutorial.secondlist.dontKnow.body">
               Choisis "je ne sais pas", on gère
             </Trans>
-          </p>
-        </Card>
+          }
+        />
 
-        <Card
-          tag="li"
-          className="shadow-primary-100 col-span-1 max-w-full flex-col justify-center rounded-2xl border-0 text-center text-sm shadow-lg drop-shadow-xs md:w-56 md:text-base">
-          <Emoji className="text-3xl">⚡️</Emoji>
-
-          <p>
-            <strong>
-              <Trans
-                locale={locale}
-                i18nKey="youthTutorial.secondlist.fast.title">
-                Rapide et utile
-              </Trans>
-            </strong>
-          </p>
-
-          <p>
-            <Trans locale={locale} i18nKey="youthTutorial.secondlist.fast.body">
-              5 minutes pour tout comprendre
+        <MiniCard
+          emoji={'⚡️'}
+          title={
+            <Trans
+              locale={locale}
+              i18nKey="youthTutorial.secondlist.fast.title">
+              Rapide et utile
             </Trans>
-          </p>
-        </Card>
+          }
+          subtitle={
+            <Trans locale={locale} i18nKey="youthTutorial.secondlist.fast.body">
+              Quelques minutes pour tout comprendre
+            </Trans>
+          }
+        />
 
-        <Card
-          tag="li"
-          className="shadow-primary-100 col-span-1 max-w-full flex-col justify-center rounded-2xl border-0 text-center text-sm shadow-lg drop-shadow-xs md:w-56 md:text-base">
-          <Emoji className="text-3xl">🏆</Emoji>
-
-          <p>
-            <strong>
-              <Trans
-                locale={locale}
-                i18nKey="youthTutorial.secondlist.result.title">
-                Résultat + actions
-              </Trans>
-            </strong>
-          </p>
-
-          <p>
+        <MiniCard
+          emoji={'🏆'}
+          title={
+            <Trans
+              locale={locale}
+              i18nKey="youthTutorial.secondlist.result.title">
+              Résultat + actions
+            </Trans>
+          }
+          subtitle={
             <Trans
               locale={locale}
               i18nKey="youthTutorial.secondlist.result.body">
               Ton score et des idées pour agir
             </Trans>
-          </p>
-        </Card>
+          }
+        />
       </ul>
 
       <div className="order-0 mb-8 text-center md:mb-0">
@@ -179,9 +179,9 @@ export default function YouthTutorial({ locale }: Props) {
           </Trans>
         </ButtonLink>
 
-        <p id="subtitle-cta" className="mt-1 text-sm text-slate-700">
+        <p id="subtitle-cta" className="mt-2 text-sm text-slate-700">
           <Trans locale={locale} i18nKey="youthTutorial.cta.helper">
-            Ça prend seulement 5 minutes
+            Ça prend seulement quelques minutes
           </Trans>{' '}
           <Emoji>⏱️</Emoji>
         </p>
