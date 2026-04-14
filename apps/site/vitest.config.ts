77c4cc19e930deb2c5a3d370b124104f8a0c7350
Johan Girod <dev@johangirod.com>
2026-04-14 16:09:00 +0200

    ✅ Fix client test

--- apps/site/vitest.config.ts
+++ apps/site/vitest.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: ['./vitest.setup.ts'],
     css: true,
-    exclude: ['**/node_modules/**', '**/e2e/**'],
+    exclude: ['**/node_modules/**', '**/e2e/**', '**/.next/**'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
