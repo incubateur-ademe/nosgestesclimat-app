65b8fe1e996c0ea0b5413976afebf31f3a2a734d
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2026-04-14 18:18:13 +0200

    ✨ Add new step for poll creation

--- apps/site/src/app/[locale]/(server)/(large)/organisations/[orgaSlug]/creer-campagne/_components/PollNameForm.tsx
+++ apps/site/src/app/[locale]/(server)/(large)/organisations/[orgaSlug]/creer-campagne/_components/PollNameForm.tsx
@@ -6,10 +6,11 @@ import TextInput from '@/design-system/inputs/TextInput'
 import { useCreatePoll } from '@/hooks/organisations/polls/useCreatePoll'
 import { useClientTranslation } from '@/hooks/useClientTranslation'
 import type { Organisation } from '@/types/organisations'
+import { safeSessionStorage } from '@/utils/browser/safeSessionStorage'
 import { captureException } from '@sentry/nextjs'
 import { useRouter } from 'next/navigation'
 import { useForm as useReactHookForm } from 'react-hook-form'
-import { revalidationOrganisationPath } from './actions/revalidationOrganisationPath'
+import { POLL_DATA_KEY } from '../_constants/sessionStorage'
 
 interface Props {
   organisation: Organisation
@@ -20,7 +21,7 @@ interface Inputs {
   expectedNumberOfParticipants: number
 }
 
-export default function PollForm({ organisation }: Props) {
+export default function PollNameForm({ organisation }: Props) {
   const router = useRouter()
 
   const { t } = useClientTranslation()
@@ -37,18 +38,34 @@ export default function PollForm({ organisation }: Props) {
     isPending,
   } = useCreatePoll(organisation.slug)
 
-  async function onSubmit({ expectedNumberOfParticipants, name }: Inputs) {
+  // async function onSubmit({ expectedNumberOfParticipants, name }: Inputs) {
+  //   try {
+  //     const pollCreated = await createPoll({
+  //       name,
+  //       expectedNumberOfParticipants: expectedNumberOfParticipants || undefined,
+  //     })
+
+  //     revalidationOrganisationPath(organisation.slug)
+
+  //     router.push(
+  //       `/organisations/${organisation.slug}/campagnes/${pollCreated.slug}`
+  //     )
+  //   } catch (error) {
+  //     captureException(error)
+  //   }
+  // }
+
+  async function onSubmit(data: Inputs) {
     try {
-      const pollCreated = await createPoll({
-        name,
-        expectedNumberOfParticipants: expectedNumberOfParticipants || undefined,
-      })
-
-      revalidationOrganisationPath(organisation.slug)
-
-      router.push(
-        `/organisations/${organisation.slug}/campagnes/${pollCreated.slug}`
+      safeSessionStorage.setItem(
+        POLL_DATA_KEY,
+        JSON.stringify({
+          data,
+        })
       )
+
+      // Go to step 2
+      router.push(`/organisations/${organisation.slug}/creer-campagne/type`)
     } catch (error) {
       captureException(error)
     }
@@ -61,10 +78,14 @@ export default function PollForm({ organisation }: Props) {
         id="poll-form">
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
           <TextInput
-            label={<Trans>Nom de la campagne</Trans>}
-            placeholder={t('ex : Campagne 2024, Classe de 6ème A, etc.')}
+            label={
+              <Trans i18nKey="collectiveTest.form.name.label">
+                Choisissez un nom pour votre test collectif
+              </Trans>
+            }
+            placeholder={t('ex : Défi climat équipe RH, Classe 5ème A…')}
             {...register('name', {
-              required: t('Ce champ est requis'),
+              required: t('Veuillez renseigner un nom'),
             })}
             error={errors.name?.message}
             data-testid="poll-name-input"
@@ -74,8 +95,8 @@ export default function PollForm({ organisation }: Props) {
             label={
               <p className="mb-0 flex w-full justify-between">
                 <span>
-                  <Trans>Nombre de participants attendus</Trans>
-                </span>{' '}
+                  <Trans>Précisez le nombre de participants attendus</Trans>
+                </span>
                 <span className="text-secondary-700 font-bold italic">
                   <Trans>facultatif</Trans>
                 </span>
@@ -98,8 +119,8 @@ export default function PollForm({ organisation }: Props) {
       {isError && (
         <p className="mt-2 text-red-800">
           <Trans>
-            Une erreur s'est produite lors de la création de la campagne.
-            Veuillez réessayer.
+            Une erreur s'est produite lors de la création de votre test
+            collectif. Veuillez réessayer.
           </Trans>
         </p>
       )}
@@ -107,10 +128,11 @@ export default function PollForm({ organisation }: Props) {
       <Button
         type="submit"
         disabled={isPending}
-        data-testid="poll-create-button"
+        data-testid="poll-form-name-button"
         form="poll-form"
         className="mt-4 self-start">
-        <Trans>Lancer ma campagne</Trans>
+        <Trans>Suivant</Trans>
+        <span aria-hidden>→</span>
       </Button>
     </>
   )
