1b645e982e24255afbd230ef17e447b2fb4b9163
Andréa Maugars <andrea.maugars@gmail.com>
2026-04-29 10:20:33 +0200

    ✨ add actions feature flag NGC-3294 (#1775)
    * ♻️  rename legacy ActionPage
    
    * ✨ add feature flag for actions v2 pages
    
    * ✨ adjust flags polling interval

--- apps/site/src/instrumentation.ts
+++ apps/site/src/instrumentation.ts
@@ -7,6 +7,23 @@ export async function register() {
 
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     await import('../sentry.server.config')
+    const { posthogClient } = await import('./services/tracking/posthogServer')
+
+    async function shutdown() {
+      try {
+        await posthogClient.shutdown()
+        process.exit(0)
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.error(error)
+        process.exit(1)
+      }
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    process.on('SIGINT', shutdown)
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    process.on('SIGTERM', shutdown)
   }
 
   if (process.env.NEXT_RUNTIME === 'edge') {
