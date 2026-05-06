6f20a9c106d4931f7c4a3380e710eb0c26609591
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2026-05-06 15:47:57 +0200

    ♻️ Use useGotoNextQuestion in Navigation

--- apps/site/src/components/form/Navigation.tsx
+++ apps/site/src/components/form/Navigation.tsx
@@ -27,6 +27,7 @@ import {
   useRule,
 } from '@/publicodes-state'
 import getValueIsOverFloorOrCeiling from '@/publicodes-state/helpers/getValueIsOverFloorOrCeiling'
+import { useGotoNextQuestion } from '@/publicodes-state/hooks/useGotoNextQuestion/useGotoNextQuestion'
 import {
   trackMatomoEvent__deprecated,
   trackPosthogEvent,
@@ -96,11 +97,11 @@ export default function Navigation({
 
   const {
     gotoPrevQuestion,
-    gotoNextQuestion,
     noPrevQuestion,
     noNextQuestion,
     setCurrentQuestion,
   } = useFormState()
+  const gotoNextQuestion = useGotoNextQuestion()
 
   const {
     isMissing,
