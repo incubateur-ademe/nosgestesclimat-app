778f04204c2ef673d1c9943e81cd9a0557d877c8
Andréa Maugars <andrea.maugars@gmail.com>
2026-05-11 18:02:16 +0200

    👷 revert changes to next build

--- apps/site/scripts/import-rewrite-loader.js
+++
@@ -1,4 +0,0 @@
-/** Allows Next.js to resolve correctly esm `imports something from './something.js'` to their corresponding `.ts` or `.tsx` files */
-export default function stripExtensions(source) {
-  return source.replaceAll(/(from\s+["'].*?)(\.js)(['"];?)$/gm, '$1$3')
-}
