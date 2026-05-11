d8cf811b7386484fef671cb57411b3ef204695cf
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2026-05-11 17:13:18 +0200

    Update page.tsx

--- apps/site/src/app/[locale]/simulateur/(simulator-flow)/(root)/[root]/page.tsx
+++ apps/site/src/app/[locale]/simulateur/(simulator-flow)/(root)/[root]/page.tsx
@@ -4,7 +4,7 @@ import { useAutoSaveSimulation } from '@/hooks/simulation/useAutoSaveSimulation'
 import { useTrackSimulator } from '@/hooks/tracking/useTrackSimulator'
 import Form from './_components/Form'
 
-export default function Layout() {
+export default function Page() {
   useAutoSaveSimulation()
   useTrackSimulator()
 
