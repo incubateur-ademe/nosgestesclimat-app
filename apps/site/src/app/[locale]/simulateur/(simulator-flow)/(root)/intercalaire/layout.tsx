96b12a84980e5217c5f648a197508ba9a4e5062b
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2026-05-11 17:17:27 +0200

    Update layout.tsx

--- apps/site/src/app/[locale]/simulateur/(simulator-flow)/(root)/intercalaire/layout.tsx
+++ apps/site/src/app/[locale]/simulateur/(simulator-flow)/(root)/intercalaire/layout.tsx
@@ -1,12 +1,10 @@
 'use client'
 
 import ContentLarge from '@/components/layout/ContentLarge'
-import { SIMULATOR_PATH } from '@/constants/urls/paths'
 import type { DottedName } from '@incubateur-ademe/nosgestesclimat'
-import { usePathname, useRouter } from 'next/navigation'
+import { usePathname } from 'next/navigation'
 import TransitionButtons from './_components/TransitionButtons'
 import TransitionHeaderSection from './_components/TransitionHeaderSection'
-import { orderedCategoriesWithoutServices } from './_constants/getOrderedCategoriesWithoutServices'
 
 export default function IntercalaireLayout({
   children,
@@ -15,16 +13,7 @@ export default function IntercalaireLayout({
 }) {
   const pathname = usePathname()
 
-  const router = useRouter()
-
   const category = pathname.split('/').at(-1)
-
-  if (
-    !category ||
-    !orderedCategoriesWithoutServices.includes(category as DottedName)
-  ) {
-    router.push(SIMULATOR_PATH)
-  }
 
   return (
     <ContentLarge className="mt-8 mb-16 px-4">
