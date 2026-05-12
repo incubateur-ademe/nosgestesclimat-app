bad3337160a96e453af1e10d9aed58ceb3c80b2c
Johan Girod <dev@johangirod.com>
2026-05-12 18:53:34 +0200

    fix translations

--- apps/site/src/app/[locale]/simulateur/(simulator-flow)/bilan/page.tsx
+++ apps/site/src/app/[locale]/simulateur/(simulator-flow)/bilan/page.tsx
@@ -1,13 +1,11 @@
 'use client'
 
-import { useAutoSaveSimulation } from '@/hooks/simulation/useAutoSaveSimulation'
 import { useTrackSimulator } from '@/hooks/tracking/useTrackSimulator'
 import { useFormState } from '@/publicodes-state'
 import { useSyncQuestionWithQueryParams } from '@/publicodes-state/providers/formProvider/hooks/useCurrent'
 import Form from './_components/Form'
 
 export default function Page() {
-  useAutoSaveSimulation()
   useTrackSimulator()
   const { currentQuestion } = useFormState()
   useSyncQuestionWithQueryParams(currentQuestion)
