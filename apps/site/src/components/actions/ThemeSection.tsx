2042caf49bcad0b5fbf6736a16e61655a5e129f3
Andréa Maugars <andrea.maugars@gmail.com>
2026-05-12 17:38:54 +0200

    ✨ add missing card translation

--- apps/site/src/components/actions/ThemeSection.tsx
+++ apps/site/src/components/actions/ThemeSection.tsx
@@ -74,7 +74,7 @@ export default function ThemeSection({
         className="-mx-2 md:mx-0"
         aria-labelledby={carouselLabelId}>
         {actions.map((action) => (
-          <ActionCard key={action.id} action={action} />
+          <ActionCard key={action.id} action={action} locale={locale} />
         ))}
       </ActionsCarousel>
     </section>
