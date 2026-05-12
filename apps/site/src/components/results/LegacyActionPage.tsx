d0ab627614b5e38d906408a671bf8d86e7fbc309
Johan Girod <dev@johangirod.com>
2026-05-12 18:53:34 +0200

    fix translations

--- apps/site/src/components/results/LegacyActionPage.tsx
+++ apps/site/src/components/results/LegacyActionPage.tsx
@@ -1,4 +1,7 @@
-import type { Simulation } from '@/helpers/server/model/simulations'
+import {
+  getSimulationMode,
+  type Simulation,
+} from '@/helpers/server/model/simulations'
 import type { Locale } from '@/i18nConfig'
 import { FormProvider } from '@/publicodes-state'
 import NoResultsBlock from '../dashboard/NoResultsBlock'
@@ -12,6 +15,7 @@ interface Props {
   locale: Locale
 }
 export function LegacyActionPage({ simulations, locale }: Props) {
+  const lastSimulation = simulations.at(-1)
   return (
     <div className="mb-20 flex flex-col">
       {simulations.length <= 0 ? (
@@ -21,8 +25,10 @@ export function LegacyActionPage({ simulations, locale }: Props) {
           <ActionAutoSave />
 
           <TopBar className="mb-6" simulationMode={false} showTotal />
-
-          <MTaTerreBanner locale={locale} />
+          {lastSimulation &&
+            getSimulationMode(lastSimulation) === 'scolaire' && (
+              <MTaTerreBanner locale={locale} />
+            )}
 
           {/* Commented while displaying M ta Terre banner <ActionsTutorial /> */}
 
