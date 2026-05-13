da77b5389dc12b576ba62282c6d806d527002055
Andréa Maugars <andrea.maugars@gmail.com>
2026-05-13 11:50:41 +0200

    💄 fix section text color

--- apps/site/src/components/actions/ThemeSection.tsx
+++ apps/site/src/components/actions/ThemeSection.tsx
@@ -8,26 +8,31 @@ import ActionsCarousel from './ActionsCarousel/ActionsCarousel'
 
 const classesByTheme: Record<
   Theme['key'],
-  Record<'section' | 'emoji', string>
+  Record<'section' | 'header' | 'emoji', string>
 > = {
   transport: {
     section: 'bg-transport-50 border-transport-200',
+    header: 'text-transport-800',
     emoji: 'border-transport-200',
   },
   food: {
     section: 'bg-alimentation-50 border-alimentation-200',
+    header: 'text-alimentation-800',
     emoji: 'border-alimentation-200',
   },
   housing: {
     section: 'bg-logement-50 border-logement-200',
+    header: 'text-logement-800',
     emoji: 'border-logement-200',
   },
   misc: {
     section: 'bg-divers-50 border-divers-200',
+    header: 'text-divers-800',
     emoji: 'border-divers-200',
   },
   societal_services: {
     section: 'bg-servicessocietaux-50 border-servicessocietaux-200',
+    header: 'text-servicessocietaux-800',
     emoji: 'border-servicessocietaux-200',
   },
 }
@@ -51,11 +56,9 @@ export default function ThemeSection({
             className={`flex size-7.5 items-center justify-center rounded-lg border bg-white text-base leading-none shadow-md ${classes.emoji}`}>
             {theme.emoji}
           </Emoji>
-          <div>
-            <h2 className="mb-0 text-lg leading-6.75 font-bold">
-              {theme.title}
-            </h2>
-            <p className="text-sm leading-5.25 font-normal">
+          <div className={classes.header}>
+            <h2 className="mb-0 text-lg/normal font-bold">{theme.title}</h2>
+            <p className="text-sm/normal font-normal">
               <Trans
                 locale={locale}
                 i18nKey="actions.components.themeSection.description"
