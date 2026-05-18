f5874ec74811755e424ec013c36c6aacfe97d071
Andréa Maugars <andrea.maugars@gmail.com>
2026-05-18 19:21:41 +0200

    ✅ add tests

--- packages/core/prisma/seed.ts
+++ packages/core/prisma/seed.ts
@@ -1,20 +1,15 @@
-import { faker } from '@faker-js/faker'
 import { actionFactory } from '../src/features/actions/factories/index.ts'
 import { prisma } from '../src/prisma/client.ts'
 
 const seed = async () => {
-  await actionFactory.createList(15)
-
-  await actionFactory.params({ publishedAt: null }).createList(5)
+  await actionFactory.published().createList(15)
+  await actionFactory.draft().createList(5)
+  await actionFactory.scheduled().createList(5)
+  await actionFactory.published().pendingDeletion().createList(5)
 
   await Promise.all(
     Array.from({ length: 5 }, () =>
-      actionFactory
-        .params({
-          publishedAt: faker.date.past(),
-          deletedAt: faker.date.recent(),
-        })
-        .create()
+      actionFactory.published().deleted().create()
     )
   )
 }
