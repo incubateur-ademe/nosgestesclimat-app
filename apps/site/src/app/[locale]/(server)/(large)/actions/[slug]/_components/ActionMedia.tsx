494eb71a03c9b391519484eb16514bf94b804163
Andréa Maugars <andrea.maugars@gmail.com>
2026-05-18 15:40:34 +0200

    ♻️  rely on types for correctness (review)

--- apps/site/src/app/[locale]/(server)/(large)/actions/[slug]/_components/ActionMedia.tsx
+++ apps/site/src/app/[locale]/(server)/(large)/actions/[slug]/_components/ActionMedia.tsx
@@ -2,11 +2,7 @@
 
 import ImpactCO2Widget from '@/components/ImpactCO2Widget'
 import type { Locale } from '@/i18nConfig'
-import {
-  type ActionMedia as ActionMediaType,
-  type ImpactCO2Language,
-  ImpactCO2LanguageSchema,
-} from '@nosgestesclimat/core/features/actions/types/action-media'
+import { type ActionMedia as ActionMediaType } from '@nosgestesclimat/core/features/actions/types/action-media'
 import Image from 'next/image'
 
 interface MediaProps extends React.ComponentPropsWithoutRef<'figure'> {
@@ -20,8 +16,6 @@ export function ActionMedia({
   className,
   ...props
 }: MediaProps) {
-  const impactCO2Language = getImpactCO2Language(locale)
-
   return (
     <figure className={className} {...props}>
       {(() => {
@@ -32,7 +26,7 @@ export function ActionMedia({
               <div className="[&_iframe]:-my-4! md:[&_iframe]:-my-8!">
                 <ImpactCO2Widget
                   type={media.data.type}
-                  language={impactCO2Language}
+                  language={locale}
                   options={media.data.options}
                 />
               </div>
@@ -59,9 +53,4 @@ export function ActionMedia({
       </figcaption>
     </figure>
   )
-}
-
-function getImpactCO2Language(locale: Locale): ImpactCO2Language {
-  const result = ImpactCO2LanguageSchema.safeParse(locale)
-  return result.success ? result.data : 'en'
 }
