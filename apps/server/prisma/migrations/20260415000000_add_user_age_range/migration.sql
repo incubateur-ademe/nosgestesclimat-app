86803d4f403f3dfd4d5a1d879d7627a65d30ff1f
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2026-05-25 15:36:35 +0200

    Delete migration.sql

--- apps/server/prisma/migrations/20260415000000_add_user_age_range/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "ngc"."AgeRange" AS ENUM ('under_18', '18-24', '25-34', '35-49', '50-64', 'over_65', 'undisclosed');
-
--- AlterTable
-ALTER TABLE "ngc"."User" ADD COLUMN "ageRange" "ngc"."AgeRange";
