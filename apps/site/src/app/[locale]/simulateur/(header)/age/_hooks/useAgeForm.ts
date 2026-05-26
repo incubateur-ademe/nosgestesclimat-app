b727836a131d66a8375179bbe71b1e4f4d6b34de
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2026-05-26 15:49:04 +0200

    Update useAgeForm.ts

--- apps/site/src/app/[locale]/simulateur/(header)/age/_hooks/useAgeForm.ts
+++ apps/site/src/app/[locale]/simulateur/(header)/age/_hooks/useAgeForm.ts
@@ -1,4 +1,3 @@
-import { useUpdateUserAgeRange } from '@/app/[locale]/simulateur/(header)/age/_hooks/useUpdateUserAgeRange'
 import { SIMULATOR_PATH } from '@/constants/urls/paths'
 import { useUser } from '@/publicodes-state'
 import {
@@ -8,50 +7,51 @@ import {
 import type { TFunction } from 'i18next'
 import { useRouter } from 'next/navigation'
 import { useState } from 'react'
+import { useUpdateUserAgeRange } from './useUpdateUserAgeRange'
 
 interface Props {
   t: TFunction
   defaultValue: AgeRange | null
 }
 
+const AGE_RANGE_LABEL_CONFIG: Record<
+  AgeRange,
+  { tKey: string; defaultLabel: string }
+> = {
+  under_18: {
+    tKey: 'simulator.age.options.moinsDe18Ans',
+    defaultLabel: 'Moins de 18 ans',
+  },
+  '18-24': {
+    tKey: 'simulator.age.options.18-24',
+    defaultLabel: '18 - 24 ans',
+  },
+  '25-34': {
+    tKey: 'simulator.age.options.25-34',
+    defaultLabel: '25 - 34 ans',
+  },
+  '35-49': {
+    tKey: 'simulator.age.options.35-49',
+    defaultLabel: '35 - 49 ans',
+  },
+  '50-64': {
+    tKey: 'simulator.age.options.50-64',
+    defaultLabel: '50 - 64 ans',
+  },
+  over_65: {
+    tKey: 'simulator.age.options.65EtPlus',
+    defaultLabel: '65 ans et plus',
+  },
+  undisclosed: {
+    tKey: 'simulator.age.options.nonPrecise',
+    defaultLabel: 'Je préfère ne pas le préciser ❌',
+  },
+}
+
 export function useAgeForm({ t, defaultValue }: Props) {
   const router = useRouter()
   const { user } = useUser()
   const { mutateAsync: updateUser, isPending } = useUpdateUserAgeRange()
-
-  const AGE_RANGE_LABEL_CONFIG: Record<
-    AgeRange,
-    { tKey: string; defaultLabel: string }
-  > = {
-    under_18: {
-      tKey: 'simulator.age.options.moinsDe18Ans',
-      defaultLabel: 'Moins de 18 ans',
-    },
-    '18-24': {
-      tKey: 'simulator.age.options.18-24',
-      defaultLabel: '18 - 24 ans',
-    },
-    '25-34': {
-      tKey: 'simulator.age.options.25-34',
-      defaultLabel: '25 - 34 ans',
-    },
-    '35-49': {
-      tKey: 'simulator.age.options.35-49',
-      defaultLabel: '35 - 49 ans',
-    },
-    '50-64': {
-      tKey: 'simulator.age.options.50-64',
-      defaultLabel: '50 - 64 ans',
-    },
-    over_65: {
-      tKey: 'simulator.age.options.65EtPlus',
-      defaultLabel: '65 ans et plus',
-    },
-    undisclosed: {
-      tKey: 'simulator.age.options.nonPrecise',
-      defaultLabel: 'Je préfère ne pas le préciser ❌',
-    },
-  }
 
   const AGE_OPTIONS = (Object.keys(AgeRangeSchema.enum) as AgeRange[]).map(
     (value) => ({
