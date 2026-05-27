67d986d9af18aeb9747362f1678dc6aac0bc8a4a
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2026-05-27 11:39:26 +0200

    add feature flag in type, use custom hook for feature flag

--- apps/site/src/app/[locale]/simulateur/(header)/tutoriel/_components/ButtonNext.tsx
+++ apps/site/src/app/[locale]/simulateur/(header)/tutoriel/_components/ButtonNext.tsx
@@ -4,19 +4,19 @@ import Trans from '@/components/translation/trans/TransClient'
 import { AGE_RANGE_KEY } from '@/constants/ab-test'
 import { AGE_PAGE_PATH, SIMULATOR_PATH } from '@/constants/urls/paths'
 import ButtonLink from '@/design-system/buttons/ButtonLink'
-import posthog from 'posthog-js'
+import { useFeatureFlag } from '@/hooks/useFeatureFlag'
 
 interface Props {
   hasSelectedAgeRange?: boolean
 }
 
 export default function ButtonNext({ hasSelectedAgeRange }: Props) {
-  const isTestVersion =
-    posthog.getFeatureFlag(AGE_RANGE_KEY) && !hasSelectedAgeRange
+  const redirectToAgePage =
+    useFeatureFlag(AGE_RANGE_KEY) && !hasSelectedAgeRange
 
   return (
     <ButtonLink
-      href={isTestVersion ? AGE_PAGE_PATH : SIMULATOR_PATH}
+      href={redirectToAgePage ? AGE_PAGE_PATH : SIMULATOR_PATH}
       data-testid="skip-tutorial-button"
       className="min-w-42!">
       <Trans i18nKey="simulator.tutorial.letsGoButton.label">
--- apps/site/src/services/feature-flags/flags.ts
+++ apps/site/src/services/feature-flags/flags.ts
@@ -1,3 +1,9 @@
-export const FEATURE_FLAGS = ['actions-v2', 'mode-scolaire'] as const
+import { AGE_RANGE_KEY } from '@/constants/ab-test'
+
+export const FEATURE_FLAGS = [
+  'actions-v2',
+  'mode-scolaire',
+  AGE_RANGE_KEY,
+] as const
 
 export type FeatureFlagName = (typeof FEATURE_FLAGS)[number]
