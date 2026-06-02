76b6b906b159105fcc61e18b0e546a01c41f9c42
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2026-06-02 11:02:37 +0200

    improve logic + extract refacto

--- apps/site/src/helpers/server/dal/middleware.ts
+++ apps/site/src/helpers/server/dal/middleware.ts
@@ -1,39 +1,13 @@
 import { getIronSession } from 'iron-session'
 import type { NextRequest, NextResponse } from 'next/server'
 import { randomUUID } from 'node:crypto'
-import {
-  getGeolocation,
-  supportedRegions,
-  type UserRegion,
-} from '../model/models'
-import { getRegionFromSearchParams } from './_helpers/getRegionFromParams'
+import { resolveRegion } from './_helpers/resolveRegion'
 import {
   type AnonSessionData,
   anonSessionOptions,
   getAnonSession,
 } from './anonSession'
 import { ANON_USER_ID_HEADER } from './constants'
-
-async function resolveRegion(
-  currentRegion: UserRegion | undefined,
-  currentInitialRegion: UserRegion | undefined,
-  searchParams: URLSearchParams
-): Promise<{ region: UserRegion; initialRegion: UserRegion | undefined }> {
-  let region: UserRegion | undefined = currentRegion
-  let initialRegion: UserRegion | undefined = currentInitialRegion
-
-  if (!region || !(region in supportedRegions)) {
-    region = await getGeolocation()
-    initialRegion = region
-  }
-
-  const forcedRegion = getRegionFromSearchParams(searchParams)
-  if (forcedRegion) {
-    region = forcedRegion
-  }
-
-  return { region, initialRegion }
-}
 
 /**
  * Middleware that ensures an encrypted anonymous session cookie exists for
