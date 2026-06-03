4b7f0e90503e30da324472f3e49e4aa469fa0229
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2026-06-03 17:34:59 +0200

    Update revalidate-age-page.ts

--- apps/site/src/services/users/revalidate-age-page.ts
+++ apps/site/src/services/users/revalidate-age-page.ts
@@ -3,6 +3,7 @@
 import { AGE_PAGE_PATH } from '@/constants/urls/paths'
 import { revalidatePath } from 'next/cache'
 
+// eslint-disable-next-line @typescript-eslint/require-await
 export async function revalidateAgePage() {
   revalidatePath(AGE_PAGE_PATH)
 }
