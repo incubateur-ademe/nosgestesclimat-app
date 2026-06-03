c7a511d92da3df35ed5b9630982858bf0e83eac7
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2026-06-03 17:28:39 +0200

    Update revalidate-age-page.ts

--- apps/site/src/services/users/revalidate-age-page.ts
+++ apps/site/src/services/users/revalidate-age-page.ts
@@ -1,7 +1,7 @@
 'use server'
 
-import { revalidatePath } from 'next/cache'
 import { AGE_PAGE_PATH } from '@/constants/urls/paths'
+import { revalidatePath } from 'next/cache'
 
 export async function revalidateAgePage() {
   revalidatePath(AGE_PAGE_PATH)
