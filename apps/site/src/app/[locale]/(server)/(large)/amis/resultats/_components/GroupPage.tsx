2049b2d939a7fa2aa9a97315815952901e157440
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2026-06-03 15:56:37 +0200

    pass GroupPage to server

--- apps/site/src/app/[locale]/(server)/(large)/amis/resultats/_components/GroupPage.tsx
+++ apps/site/src/app/[locale]/(server)/(large)/amis/resultats/_components/GroupPage.tsx
@@ -1,25 +1,43 @@
-'use client'
-
+import CategoriesAccordion from '@/components/results/CategoriesAccordion'
+import { carboneMetric } from '@/constants/model/metric'
+import { getCachedRules } from '@/helpers/modelFetching/getCachedRules'
+import type { Simulation } from '@/helpers/server/model/simulations'
+import type { Locale } from '@/i18nConfig'
 import type { Group } from '@/types/groups'
-import type { ReactNode } from 'react'
 import EditableGroupTitle from './EditableGroupTitle'
 import GroupResults from './GroupResults'
 import UpdateSimulationUsed from './UpdateSimulationUsed'
 
-export default function GroupPage({
-  group,
-  categoriesAccordion,
-}: {
+interface Props {
   group: Group
-  categoriesAccordion?: ReactNode
-}) {
+  locale: Locale
+  userSimulation: Simulation
+}
+
+export default async function GroupPage({
+  group,
+  locale,
+  userSimulation,
+}: Props) {
+  const rules = await getCachedRules({ locale })
+
   return (
     <>
       <EditableGroupTitle group={group} />
 
       <UpdateSimulationUsed group={group} />
 
-      <GroupResults group={group} categoriesAccordion={categoriesAccordion} />
+      <GroupResults
+        group={group}
+        categoriesAccordion={
+          <CategoriesAccordion
+            locale={locale}
+            rules={rules}
+            computedResults={userSimulation.computedResults}
+            metric={carboneMetric}
+          />
+        }
+      />
     </>
   )
 }
--- apps/site/src/app/[locale]/(server)/(large)/amis/resultats/page.tsx
+++ apps/site/src/app/[locale]/(server)/(large)/amis/resultats/page.tsx
@@ -1,11 +1,8 @@
-import CategoriesAccordion from '@/components/results/CategoriesAccordion'
-import { carboneMetric } from '@/constants/model/metric'
 import {
   END_PAGE_GROUPS_PATH,
   MON_ESPACE_GROUPS_PATH,
 } from '@/constants/urls/paths'
 import GoBackLink from '@/design-system/inputs/GoBackLink'
-import { getCachedRules } from '@/helpers/modelFetching/getCachedRules'
 import type { DefaultPageProps } from '@/types'
 import GroupPage from './_components/GroupPage'
 import { groupResultsGuard } from './guard'
@@ -16,10 +13,7 @@ export default async function GroupResultsPage({
 }: DefaultPageProps<{ searchParams: Promise<{ groupId: string }> }>) {
   const locale = (await params).locale
 
-  const [{ user, userSimulation, group }, rules] = await Promise.all([
-    groupResultsGuard(searchParams),
-    getCachedRules({ locale }),
-  ])
+  const { user, group, userSimulation } = await groupResultsGuard(searchParams)
 
   return (
     <div className="pb-8">
@@ -29,14 +23,8 @@ export default async function GroupResultsPage({
       />
       <GroupPage
         group={group}
-        categoriesAccordion={
-          <CategoriesAccordion
-            locale={locale}
-            rules={rules}
-            computedResults={userSimulation.computedResults}
-            metric={carboneMetric}
-          />
-        }
+        locale={locale}
+        userSimulation={userSimulation}
       />
     </div>
   )
