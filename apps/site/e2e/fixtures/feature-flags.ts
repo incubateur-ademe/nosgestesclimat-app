32f0e0c8d68a434b5872e70f5a136200a569b233
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2026-06-04 10:05:06 +0200

    fix e2e test

--- apps/site/e2e/fixtures/feature-flags.ts
+++ apps/site/e2e/fixtures/feature-flags.ts
@@ -5,7 +5,7 @@ import type { DefaultFlagValues } from '@/services/feature-flags/flags'
 
 const DOMAIN = new URL(process.env.NEXT_PUBLIC_SITE_URL!).hostname
 
-const DEFAULT_FLAGS = {
+export const DEFAULT_FLAGS = {
   'actions-v2': false,
   'mode-scolaire': true,
   'ab-test-question-tranche-dage': 'control',
