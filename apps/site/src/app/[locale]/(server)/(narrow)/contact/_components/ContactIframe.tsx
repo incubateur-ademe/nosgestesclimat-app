c6febe786a9d978f81c23c7250a85e15bdfb42ca
Benjamin Arias <bjlaa@proton.me>
2026-06-16 16:44:57 +0200

    ✨ Display english contact form (#1853)
    Co-authored-by: Benjamin Arias <12382534+bjlaa@users.noreply.github.com>

--- apps/site/src/app/[locale]/(server)/(narrow)/contact/_components/ContactIframe.tsx
+++ apps/site/src/app/[locale]/(server)/(narrow)/contact/_components/ContactIframe.tsx
@@ -1,17 +1,23 @@
 'use client'
 
 import { useClientTranslation } from '@/hooks/useClientTranslation'
+import { useLocale } from '@/hooks/useLocale'
 
 export function ContactIframe() {
   const { t } = useClientTranslation()
 
+  const locale = useLocale()
+
   return (
     <iframe
-      data-tally-src="https://tally.so/embed/w59G1Z?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
+      data-tally-src={`https://tally.so/embed/${locale === 'fr' ? 'w59G1Z' : 'gDZ00J'}?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1`}
       loading="lazy"
       id="iframe-contact"
       // This title is overwritten by Tally
-      title={t('Formulaire - nous faire part de vos remarques sur le site')}
+      title={t(
+        'contact.iframe.title.fr',
+        'Formulaire - nous faire part de vos remarques sur le site'
+      )}
       onLoad={() => {
         if (typeof window === 'undefined') return
 
@@ -19,7 +25,15 @@ export function ContactIframe() {
         if (iframe) {
           iframe.setAttribute(
             'title',
-            t('Formulaire - nous faire part de vos remarques sur le site')
+            locale === 'fr'
+              ? t(
+                  'contact.iframe.title.fr',
+                  'Formulaire - nous faire part de vos remarques sur le site'
+                )
+              : t(
+                  'contact.iframe.title.en',
+                  'Formulaire - give some feedback about Nos Gestes Climat'
+                )
           )
         }
       }}></iframe>
