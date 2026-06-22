9ef6a8e99e53948072e1895d457ed272a65f3604
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2026-06-22 15:47:18 +0200

    Update shareDataWithIntegrator.ts

--- apps/site/src/helpers/iframe/shareDataWithIntegrator.ts
+++ apps/site/src/helpers/iframe/shareDataWithIntegrator.ts
@@ -3,30 +3,22 @@ import type { ComputedResults } from '@/publicodes-state/types'
 import { postMessageToIntegrator } from './postMessageToIntegrator'
 
 export function shareDataWithIntegrator(computedResults: ComputedResults) {
-  try {
-    const sharedData = {
-      t: computedResults[carboneMetric].categories.transport,
-      a: computedResults[carboneMetric].categories.alimentation,
-      l: computedResults[carboneMetric].categories.logement,
-      d: computedResults[carboneMetric].categories.divers,
-      s: computedResults[carboneMetric].categories['services sociétaux'],
-      footprints: {
-        carbon: computedResults[carboneMetric],
-        water: computedResults[eauMetric],
-      },
-    }
-
-    const message = {
-      messageType: 'ngc-iframe-share',
-      data: sharedData,
-    }
-
-    postMessageToIntegrator(message)
-  } catch (error) {
-    // eslint-disable-next-line
-    console.log(
-      'NGC - shareDataWithIntegrator - Error when posting message to React Native',
-      error
-    )
+  const sharedData = {
+    t: computedResults[carboneMetric].categories.transport,
+    a: computedResults[carboneMetric].categories.alimentation,
+    l: computedResults[carboneMetric].categories.logement,
+    d: computedResults[carboneMetric].categories.divers,
+    s: computedResults[carboneMetric].categories['services sociétaux'],
+    footprints: {
+      carbon: computedResults[carboneMetric],
+      water: computedResults[eauMetric],
+    },
   }
+
+  const message = {
+    messageType: 'ngc-iframe-share',
+    data: sharedData,
+  }
+
+  postMessageToIntegrator(message)
 }
