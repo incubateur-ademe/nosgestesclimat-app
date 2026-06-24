a58f55dd59eb485ffa4201b77d84a117a5ae6da5
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2026-06-24 10:33:46 +0200

    Update verifyIntegratorBypassRights.ts

--- apps/site/src/app/[locale]/_components/mainLayoutProviders/_helpers/verifyIntegratorBypassRights.ts
+++ apps/site/src/app/[locale]/_components/mainLayoutProviders/_helpers/verifyIntegratorBypassRights.ts
@@ -2,7 +2,7 @@
 /**
  * Verify if the integrator is allowed to bypass the consent data share
  */
-export const verifyIfIntegratorBypassRights = (
+export const verifyIfIntegratorBypassRights = async (
   bypassKey: string
 ): Promise<boolean> => {
   return Promise.resolve(!!bypassKey && process.env.BYPASS_KEY === bypassKey)
