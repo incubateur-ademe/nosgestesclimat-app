a62014fb584cbae768d829058ba23a0bd65b1e61
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2026-06-29 15:09:51 +0200

    update tests

--- apps/server/src/core/__tests__/fixtures/server.fixture.ts
+++ apps/server/src/core/__tests__/fixtures/server.fixture.ts
@@ -1,7 +1,14 @@
+import { HttpHandler } from 'msw'
 import { setupServer } from 'msw/node'
+import { expect } from 'vitest'
 
 export const mswServer = setupServer()
 
 export const resetMswServer = () => {
+  for (const handler of mswServer.listHandlers()) {
+    if (handler instanceof HttpHandler) {
+      expect(handler.isUsed).toBeTruthy()
+    }
+  }
   mswServer.resetHandlers()
 }
