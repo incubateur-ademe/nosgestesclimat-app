fe65455eac2d65c8bd9045577bb43f0c7510ab92
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2026-06-30 16:24:59 +0200

    ✨ Add illustrations

--- apps/site/src/app/[locale]/(server)/(large)/organisations/[orgaSlug]/campagnes/[pollSlug]/_components/CommunicationKit.tsx
+++ apps/site/src/app/[locale]/(server)/(large)/organisations/[orgaSlug]/campagnes/[pollSlug]/_components/CommunicationKit.tsx
@@ -3,10 +3,11 @@
 import Link from '@/components/Link'
 import Trans from '@/components/translation/trans/TransClient'
 import ButtonLink from '@/design-system/buttons/ButtonLink'
+import Image from 'next/image'
 
 export default function CommunicationKit() {
   return (
-    <div className="flex border-t border-slate-300 py-8 md:gap-10">
+    <div className="relative my-8 flex flex-col items-stretch gap-10 border-t border-slate-300 py-8 md:flex-row">
       <div>
         <h2>
           <Trans i18nKey="poll.results.communicationKit.title">
@@ -25,7 +26,7 @@ export default function CommunicationKit() {
             </Link>{' '}
             <strong>prêt à l’emploi</strong> pour tous les temps forts de
             l’année, pour encourager le plus de personnes possibles à{' '}
-            <strong>rejoindre le challenge</strong>et venir{' '}
+            <strong>rejoindre le challenge</strong> et venir{' '}
             <strong>calculer leur empreinte</strong>.
           </Trans>
         </p>
@@ -45,7 +46,26 @@ export default function CommunicationKit() {
         </ButtonLink>
       </div>
 
-      <div></div>
+      <div className="relative mx-auto h-70 w-60 md:mx-0 md:mr-16 md:min-w-75">
+        <div className="absolute inset-0">
+          <Image
+            src="https://nosgestesclimat-prod.s3.fr-par.scw.cloud/cms/illu_1_kit_communication_a6321fe7ec.png"
+            width="200"
+            height="200"
+            className="border border-slate-300 object-contain"
+            alt=""
+          />
+        </div>
+        <div className="absolute top-1/2 right-0 z-10 -translate-y-1/2">
+          <Image
+            src="https://nosgestesclimat-prod.s3.fr-par.scw.cloud/cms/illu_2_kit_communication_e7769e5e6b.png"
+            width="150"
+            height="150"
+            alt=""
+            className="border border-slate-300 shadow-lg"
+          />
+        </div>
+      </div>
     </div>
   )
 }
