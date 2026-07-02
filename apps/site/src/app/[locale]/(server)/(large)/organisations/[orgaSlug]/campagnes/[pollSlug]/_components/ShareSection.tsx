d0b1336691e1328a02d2bad87afcd7d34416c5a7
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2026-07-02 09:59:40 +0200

    🎨 Remove buttons from share section

--- apps/site/src/app/[locale]/(server)/(large)/organisations/[orgaSlug]/campagnes/[pollSlug]/_components/ShareSection.tsx
+++ apps/site/src/app/[locale]/(server)/(large)/organisations/[orgaSlug]/campagnes/[pollSlug]/_components/ShareSection.tsx
@@ -1,19 +1,9 @@
 'use client'
 
-import FacebookIcon from '@/components/icons/share/FacebookIcon'
-import LinkedinIcon from '@/components/icons/share/LinkedinIcon'
 import MailIcon from '@/components/icons/share/MailIcon'
-import MessengerIcon from '@/components/icons/share/MessengerIcon'
-import WhatsappIcon from '@/components/icons/share/WhatsappIcon'
 import Link from '@/components/Link'
 import QRCode from '@/components/sharing/QRCode'
 import Trans from '@/components/translation/trans/TransClient'
-import {
-  FACEBOOK_SHARE_URL,
-  LINKEDIN_SHARE_URL,
-  MESSENGER_SHARE_MOBILE_URL,
-  WHATSAPP_SHARE_URL,
-} from '@/constants/urls/share'
 import { UTM_MEDIUM_KEY, UTM_SOURCE_KEY } from '@/constants/urls/utm'
 import ButtonLink from '@/design-system/buttons/ButtonLink'
 import CopyButton from '@/design-system/buttons/CopyButton'
@@ -97,87 +87,36 @@ export default function ShareSection({ poll, className, title }: Props) {
         </Card>
       </div>
 
-      <div className="mt-12 mb-16 flex flex-col items-start">
-        <h3>
-          <Trans i18nKey="pollResults.adminSection.shareDirectly">
-            Ou partagez directement
-          </Trans>
-        </h3>
-
+      <div className="mt-12 mb-16 flex flex-col gap-6 md:flex-row md:items-center">
         <QRCode
           value={buildLink({
             orgaSlug,
             pollSlug,
           })}
-          className="md:flex-1"
         />
 
-        <ul className="mt-6 flex w-full flex-wrap gap-2">
-          {[
-            {
-              icon: <WhatsappIcon />,
-              label: t('Whatsapp'),
-              link: `${WHATSAPP_SHARE_URL}${url}`,
-              eventTracked: getShareTrackEvent({
-                page: 'Fin',
-                target: 'Whatsapp',
-              }),
-            },
-            {
-              icon: <FacebookIcon />,
-              label: t('Facebook'),
-              link: `${FACEBOOK_SHARE_URL}${url}`,
-              eventTracked: getShareTrackEvent({
-                page: 'Fin',
-                target: 'Facebook',
-              }),
-            },
-            {
-              icon: <MessengerIcon />,
-              label: t('Messenger'),
-              link: `${MESSENGER_SHARE_MOBILE_URL}${url}`,
-              mobileOnly: true,
-              eventTracked: getShareTrackEvent({
-                page: 'Fin',
-                target: 'Messenger',
-              }),
-            },
-            {
-              icon: <LinkedinIcon />,
-              label: t('Linkedin'),
-              link: `${LINKEDIN_SHARE_URL}${url}`,
-              eventTracked: getShareTrackEvent({
-                page: 'Fin',
-                target: 'Linkedin',
-              }),
-            },
-            {
-              icon: <MailIcon className="fill-primary-700 w-4" />,
-              label: t('Envoyer par e-mail'),
-              link: `mailto:?subject=${t('Voici mes empreintes carbone et eau ; tu connais les tiennes ?')}&body=${url}`,
-              eventTracked: getShareTrackEvent({
+        <ButtonLink
+          data-testid="share-button-link-0"
+          className="max-h-10 w-full! flex-1 py-5 text-sm! md:w-auto md:max-w-48"
+          color="secondary"
+          target="_blank"
+          rel="noopener noreferrer"
+          href={`mailto:?subject=${t('Voici mes empreintes carbone et eau ; tu connais les tiennes ?')}&body=${url}`}
+          onClick={() =>
+            trackMatomoEvent__deprecated(
+              getShareTrackEvent({
                 page: 'Fin',
                 target: 'E-mail',
-              }),
-            },
-          ].map(({ link, label, icon, eventTracked }, index) => (
-            <li key={link} className="flex max-w-48 flex-1">
-              <ButtonLink
-                data-testid={`share-button-link-${index}`}
-                className="max-h-10 w-auto max-w-48 flex-1 text-sm!"
-                color="secondary"
-                target="_blank"
-                rel="noopener noreferrer"
-                href={link}
-                onClick={() => trackMatomoEvent__deprecated(eventTracked)}>
-                <span className="flex items-center gap-2">
-                  {label}
-                  {icon}
-                </span>
-              </ButtonLink>
-            </li>
-          ))}
-        </ul>
+              })
+            )
+          }>
+          <span className="flex items-center gap-2">
+            <Trans i18nKey="poll.shareSection.sendByEmail">
+              Envoyer par e-mail
+            </Trans>
+            <MailIcon className="fill-primary-700 w-4" />
+          </span>
+        </ButtonLink>
       </div>
     </section>
   )
