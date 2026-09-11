incubateur-ademe/nosgestesclimat-site-nextjs

♻️ Change following code review

packages/core/src/features/polls/stats/legacy/evaluate-situation.ts
@@ -4,7 +4,6 @@
  * engine-based evaluation in Phase 2.
  */
 import type { DottedName, NGCRules } from '@incubateur-ademe/nosgestesclimat'
-import type { Logger } from '../../../logger/index.ts'
 import type { SituationSchema } from './situation.schema.ts'
 
 const isDottedName = (dottedName: unknown): dottedName is DottedName =>
@@ -38,19 +37,22 @@ const isOperator = (raw: string): raw is Operator => OPERATORS.has(raw)
 
 const evaluateConditions = (
   operator: Operator,
-  { left, right }: { left: number | string; right: number | string }
+  {
+    left,
+    right,
+  }: { left: number; right: number } | { left: string; right: string }
 ) => {
   switch (operator) {
     case '<':
-      return (left as number) < (right as number)
+      return left < right
     case '>':
-      return (left as number) > (right as number)
+      return left > right
     case '=':
       return left === right
     case '>=':
-      return (left as number) >= (right as number)
+      return left >= right
     case '<=':
-      return (left as number) <= (right as number)
+      return left <= right
     case '!=':
       return left !== right
   }
@@ -91,6 +93,7 @@ const checkIfConditionIsTrue = ({
 
   if (
     !dottedName ||
+    // if the dottedName is not in the situation (default or no show) we consider the condition as false
     situation[dottedName] === undefined ||
     situation[dottedName] === null
   ) {
@@ -104,8 +107,10 @@ const checkIfConditionIsTrue = ({
   const left =
     typeof situation[dottedName] === 'string'
       ? situation[dottedName]
-      : evaluateSituationDottedName({ dottedName, situation })
-
+      : evaluateSituationDottedName({
+          dottedName,
+          situation,
+        })
   if (isOperator(operator) && value) {
     if (Number.isNaN(+value)) {
       return typeof left === 'string'
@@ -148,6 +153,7 @@ const evaluateSituationFormula = ({
             rules,
           })
         }
+
         return +variation.alors || 0
       }
     }
@@ -155,6 +161,8 @@ const evaluateSituationFormula = ({
     formule = fallback.sinon
 
     if (typeof formule === 'number') {
+      // TODO: fix me engine does not fallback correctly
+      // return formule
       return 0
     }
   }
@@ -212,43 +220,34 @@ const evaluateSituationFormula = ({
   return 0
 }
 
-export function createGetSituationDottedNameValue({
-  logger,
+// Unlike the legacy server version, errors are thrown (not caught and logged):
+// the caller is responsible for handling them.
+export const getSituationDottedNameValue = ({
+  dottedName,
+  situation,
+  rules,
 }: {
-  logger: Logger
-}) {
-  return function getSituationDottedNameValue({
-    dottedName,
+  situation: SituationSchema
+  dottedName: DottedName
+  rules: Partial<NGCRules>
+}): number => {
+  const rule = rules[dottedName]
+
+  if (
+    !rule ||
+    typeof rule === 'string' ||
+    !rule.formule ||
+    typeof rule.formule !== 'object'
+  ) {
+    if (typeof rule?.formule === 'number') {
+      return rule.formule
+    }
+    return 0
+  }
+
+  return evaluateSituationFormula({
+    formule: rule.formule,
     situation,
     rules,
-  }: {
-    situation: SituationSchema
-    dottedName: DottedName
-    rules: Partial<NGCRules>
-  }): number {
-    try {
-      const rule = rules[dottedName]
-
-      if (
-        !rule ||
-        typeof rule === 'string' ||
-        !rule.formule ||
-        typeof rule.formule !== 'object'
-      ) {
-        if (typeof rule?.formule === 'number') {
-          return rule.formule
-        }
-        return 0
-      }
-
-      return evaluateSituationFormula({
-        formule: rule.formule,
-        situation,
-        rules,
-      })
-    } catch (error) {
-      logger.error('Cannot evaluate dottedName', { dottedName, error })
-      return 0
-    }
-  }
+  })
 }

packages/core/src/features/polls/stats/services/__tests__/enqueue-poll-stats-computation.spec.ts
@@ -1,0 +1,119 @@
+import { afterEach, describe, expect, it, test } from 'vitest'
+import { prisma } from '../../../../../prisma/client.ts'
+import { pollStatsComputationFactory } from '../../../factories/poll-stats-computation.factory.ts'
+import { getPollStatsComputationStatus } from '../../repositories/poll-stats-computations.repository.ts'
+import { createEnqueuePollStatsComputation } from '../enqueue-poll-stats-computation.ts'
+
+const immediateEnqueue = createEnqueuePollStatsComputation({
+  cooldownTiers: [{ upTo: null, cooldownSeconds: 0 }],
+})
+
+const deferredEnqueue = createEnqueuePollStatsComputation({
+  cooldownTiers: [{ upTo: null, cooldownSeconds: 3600 }],
+})
+
+describe('enqueuePollStatsComputation', () => {
+  afterEach(async () => {
+    await prisma.pollStatsComputation.deleteMany()
+    await prisma.poll.deleteMany()
+    await prisma.organisation.deleteMany()
+  })
+
+  it('creates an immediate pending computation when no row exists', async () => {
+    const poll = await pollStatsComputationFactory.create()
+
+    const before = Date.now()
+    await immediateEnqueue(poll.id)
+    const after = Date.now()
+
+    const computation = await getPollStatsComputationStatus(poll.id)
+    expect(computation!.status).toBe('pending')
+    expect(computation!.scheduledAt?.getTime()).toBeGreaterThanOrEqual(before)
+    expect(computation!.scheduledAt?.getTime()).toBeLessThanOrEqual(after)
+  })
+
+  it('re-arms a completed computation immediately when the cooldown is 0s', async () => {
+    const poll = await pollStatsComputationFactory
+      .withCompletedComputation()
+      .create()
+
+    const before = Date.now()
+    await immediateEnqueue(poll.id)
+    const after = Date.now()
+
+    const computation = await getPollStatsComputationStatus(poll.id)
+    expect(computation!.status).toBe('pending')
+    expect(computation!.scheduledAt?.getTime()).toBeGreaterThanOrEqual(before)
+    expect(computation!.scheduledAt?.getTime()).toBeLessThanOrEqual(after)
+  })
+
+  it('defers a completed computation when the cooldown is large', async () => {
+    const poll = await pollStatsComputationFactory
+      .withCompletedComputation()
+      .create()
+
+    await deferredEnqueue(poll.id)
+
+    const computation = await getPollStatsComputationStatus(poll.id)
+    expect(computation!.status).toBe('pending')
+    expect(computation!.scheduledAt?.getTime()).toBeGreaterThan(
+      Date.now() + 30 * 60 * 1000
+    )
+  })
+
+  it('reschedules a failed computation to pending now', async () => {
+    const poll = await pollStatsComputationFactory
+      .withFailedComputation()
+      .create()
+
+    const before = Date.now()
+    await immediateEnqueue(poll.id)
+    const after = Date.now()
+
+    const computation = await getPollStatsComputationStatus(poll.id)
+    expect(computation!.status).toBe('pending')
+    expect(computation!.scheduledAt?.getTime()).toBeGreaterThanOrEqual(before)
+    expect(computation!.scheduledAt?.getTime()).toBeLessThanOrEqual(after)
+  })
+
+  it('keeps the scheduledAt when a deferred computation is enqueued again', async () => {
+    const poll = await pollStatsComputationFactory
+      .withCompletedComputation()
+      .create()
+
+    await deferredEnqueue(poll.id)
+    const firstScheduledAt = (await getPollStatsComputationStatus(poll.id))!
+      .scheduledAt
+
+    await deferredEnqueue(poll.id)
+
+    const computation = await getPollStatsComputationStatus(poll.id)
+    expect(computation!.status).toBe('pending')
+    expect(computation!.scheduledAt?.getTime()).toBe(
+      firstScheduledAt!.getTime()
+    )
+  })
+
+  test.each([
+    {
+      name: 'leaves a deferred pending computation untouched',
+      create: () =>
+        pollStatsComputationFactory
+          .withPendingComputation(new Date(Date.now() + 60 * 1000))
+          .create(),
+    },
+    {
+      name: 'leaves a processing computation untouched',
+      create: () =>
+        pollStatsComputationFactory.withProcessingComputation().create(),
+    },
+  ])('$name', async ({ create }) => {
+    const poll = await create()
+    const before = await getPollStatsComputationStatus(poll.id)
+
+    await immediateEnqueue(poll.id)
+
+    const after = await getPollStatsComputationStatus(poll.id)
+    expect(after).toEqual(before)
+  })
+})

incubateur-ademe/nosgestesclimat-app

packages/core/src/features/polls/stats/legacy/evaluate-situation.ts
@@ -4,7 +4,6 @@
  * engine-based evaluation in Phase 2.
  */
 import type { DottedName, NGCRules } from '@incubateur-ademe/nosgestesclimat'
-import type { Logger } from '../../../logger/index.ts'
 import type { Situation } from '../../../simulations/validators/situation.schema.ts'
 
 const isDottedName = (dottedName: unknown): dottedName is DottedName =>
@@ -38,19 +37,22 @@ const isOperator = (raw: string): raw is Operator => OPERATORS.has(raw)
 
 const evaluateConditions = (
   operator: Operator,
-  { left, right }: { left: number | string; right: number | string }
+  {
+    left,
+    right,
+  }: { left: number; right: number } | { left: string; right: string }
 ) => {
   switch (operator) {
     case '<':
-      return (left as number) < (right as number)
+      return left < right
     case '>':
-      return (left as number) > (right as number)
+      return left > right
     case '=':
       return left === right
     case '>=':
-      return (left as number) >= (right as number)
+      return left >= right
     case '<=':
-      return (left as number) <= (right as number)
+      return left <= right
     case '!=':
       return left !== right
   }
@@ -91,6 +93,7 @@ const checkIfConditionIsTrue = ({
 
   if (
     !dottedName ||
+    // if the dottedName is not in the situation (default or no show) we consider the condition as false
     situation[dottedName] === undefined ||
     situation[dottedName] === null
   ) {
@@ -104,8 +107,10 @@ const checkIfConditionIsTrue = ({
   const left =
     typeof situation[dottedName] === 'string'
       ? situation[dottedName]
-      : evaluateSituationDottedName({ dottedName, situation })
-
+      : evaluateSituationDottedName({
+          dottedName,
+          situation,
+        })
   if (isOperator(operator) && value) {
     if (Number.isNaN(+value)) {
       return typeof left === 'string'
@@ -148,6 +153,7 @@ const evaluateSituationFormula = ({
             rules,
           })
         }
+
         return +variation.alors || 0
       }
     }
@@ -155,6 +161,8 @@ const evaluateSituationFormula = ({
     formule = fallback.sinon
 
     if (typeof formule === 'number') {
+      // TODO: fix me engine does not fallback correctly
+      // return formule
       return 0
     }
   }
@@ -212,43 +220,34 @@ const evaluateSituationFormula = ({
   return 0
 }
 
-export function createGetSituationDottedNameValue({
-  logger,
+// Unlike the legacy server version, errors are thrown (not caught and logged):
+// the caller is responsible for handling them.
+export const getSituationDottedNameValue = ({
+  dottedName,
+  situation,
+  rules,
 }: {
-  logger: Logger
-}) {
-  return function getSituationDottedNameValue({
-    dottedName,
+  situation: Situation
+  dottedName: DottedName
+  rules: Partial<NGCRules>
+}): number => {
+  const rule = rules[dottedName]
+
+  if (
+    !rule ||
+    typeof rule === 'string' ||
+    !rule.formule ||
+    typeof rule.formule !== 'object'
+  ) {
+    if (typeof rule?.formule === 'number') {
+      return rule.formule
+    }
+    return 0
+  }
+
+  return evaluateSituationFormula({
+    formule: rule.formule,
     situation,
     rules,
-  }: {
-    situation: Situation
-    dottedName: DottedName
-    rules: Partial<NGCRules>
-  }): number {
-    try {
-      const rule = rules[dottedName]
-
-      if (
-        !rule ||
-        typeof rule === 'string' ||
-        !rule.formule ||
-        typeof rule.formule !== 'object'
-      ) {
-        if (typeof rule?.formule === 'number') {
-          return rule.formule
-        }
-        return 0
-      }
-
-      return evaluateSituationFormula({
-        formule: rule.formule,
-        situation,
-        rules,
-      })
-    } catch (error) {
-      logger.error('Cannot evaluate dottedName', { dottedName, error })
-      return 0
-    }
-  }
+  })
 }

packages/core/src/features/polls/stats/services/__tests__/enqueue-poll-stats-computation.spec.ts
@@ -1,0 +1,119 @@
+import { afterEach, describe, expect, it, test } from 'vitest'
+import { prisma } from '../../../../../prisma/client.ts'
+import { pollFactory } from '../../../factories/poll.factory.ts'
+import { getPollStatsComputationStatus } from '../../repositories/poll-stats-computations.repository.ts'
+import { createEnqueuePollStatsComputation } from '../enqueue-poll-stats-computation.ts'
+
+const immediateEnqueue = createEnqueuePollStatsComputation({
+  cooldownTiers: [{ upTo: null, cooldownSeconds: 0 }],
+})
+
+const deferredEnqueue = createEnqueuePollStatsComputation({
+  cooldownTiers: [{ upTo: null, cooldownSeconds: 3600 }],
+})
+
+describe('enqueuePollStatsComputation', () => {
+  afterEach(async () => {
+    await prisma.pollStatsComputation.deleteMany()
+    await prisma.poll.deleteMany()
+    await prisma.organisation.deleteMany()
+  })
+
+  it('creates an immediate pending computation when no row exists', async () => {
+    const poll = await pollFactory.create()
+
+    const before = Date.now()
+    await immediateEnqueue(poll.id)
+    const after = Date.now()
+
+    const computation = await getPollStatsComputationStatus(poll.id)
+    expect(computation!.status).toBe('pending')
+    expect(computation!.scheduledAt?.getTime()).toBeGreaterThanOrEqual(before)
+    expect(computation!.scheduledAt?.getTime()).toBeLessThanOrEqual(after)
+  })
+
+  it('re-arms a completed computation immediately when the cooldown is 0s', async () => {
+    const poll = await pollFactory
+      .withCompletedComputation()
+      .create()
+
+    const before = Date.now()
+    await immediateEnqueue(poll.id)
+    const after = Date.now()
+
+    const computation = await getPollStatsComputationStatus(poll.id)
+    expect(computation!.status).toBe('pending')
+    expect(computation!.scheduledAt?.getTime()).toBeGreaterThanOrEqual(before)
+    expect(computation!.scheduledAt?.getTime()).toBeLessThanOrEqual(after)
+  })
+
+  it('defers a completed computation when the cooldown is large', async () => {
+    const poll = await pollFactory
+      .withCompletedComputation()
+      .create()
+
+    await deferredEnqueue(poll.id)
+
+    const computation = await getPollStatsComputationStatus(poll.id)
+    expect(computation!.status).toBe('pending')
+    expect(computation!.scheduledAt?.getTime()).toBeGreaterThan(
+      Date.now() + 30 * 60 * 1000
+    )
+  })
+
+  it('reschedules a failed computation to pending now', async () => {
+    const poll = await pollFactory
+      .withFailedComputation()
+      .create()
+
+    const before = Date.now()
+    await immediateEnqueue(poll.id)
+    const after = Date.now()
+
+    const computation = await getPollStatsComputationStatus(poll.id)
+    expect(computation!.status).toBe('pending')
+    expect(computation!.scheduledAt?.getTime()).toBeGreaterThanOrEqual(before)
+    expect(computation!.scheduledAt?.getTime()).toBeLessThanOrEqual(after)
+  })
+
+  it('keeps the scheduledAt when a deferred computation is enqueued again', async () => {
+    const poll = await pollFactory
+      .withCompletedComputation()
+      .create()
+
+    await deferredEnqueue(poll.id)
+    const firstScheduledAt = (await getPollStatsComputationStatus(poll.id))!
+      .scheduledAt
+
+    await deferredEnqueue(poll.id)
+
+    const computation = await getPollStatsComputationStatus(poll.id)
+    expect(computation!.status).toBe('pending')
+    expect(computation!.scheduledAt?.getTime()).toBe(
+      firstScheduledAt!.getTime()
+    )
+  })
+
+  test.each([
+    {
+      name: 'leaves a deferred pending computation untouched',
+      create: () =>
+        pollFactory
+          .withPendingComputation(new Date(Date.now() + 60 * 1000))
+          .create(),
+    },
+    {
+      name: 'leaves a processing computation untouched',
+      create: () =>
+        pollFactory.withProcessingComputation().create(),
+    },
+  ])('$name', async ({ create }) => {
+    const poll = await create()
+    const before = await getPollStatsComputationStatus(poll.id)
+
+    await immediateEnqueue(poll.id)
+
+    const after = await getPollStatsComputationStatus(poll.id)
+    expect(after).toEqual(before)
+  })
+})