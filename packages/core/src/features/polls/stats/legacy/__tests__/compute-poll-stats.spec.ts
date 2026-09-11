incubateur-ademe/nosgestesclimat-site-nextjs

♻️ Remplace le cache Redis des stats de campagne par une file Postgres throttlée
Le calcul des stats de campagne reposait sur un cache Redis mis à jour de
façon incrémentale à chaque simulation terminée. Cette écriture non
atomique (lecture → modification → réécriture) faussait les totaux quand
plusieurs participations se terminaient en parallèle, et le flag manuel
`computeRealTimeStats` était nécessaire pour couper ce calcul sur les
grosses campagnes.

On remplace ce mécanisme par une file Postgres (`PollStatsComputation`) :
une simulation terminée marque simplement la campagne comme « à
recalculer », puis un worker recalcule les stats en entier, avec un
cooldown à trois niveaux selon la taille de la campagne. La file est
fiable en multi-instance (`FOR UPDATE SKIP LOCKED`) et le cache Redis est
supprimé de cette feature.

Au passage, on réorganise les features autour du domaine `polls`
(ex-`poll-stats`) et on resserre les frontières d'agrégats des factories
de test (`SimulationFactory`, `SimulationComputationFactory`,
`SimulationPollFactory`).

packages/core/src/features/polls/stats/legacy/__tests__/compute-poll-stats.spec.ts
@@ -1,0 +1,67 @@
+import { afterEach, describe, expect, it, vi } from 'vitest'
+import { prisma } from '../../../../../prisma/client.ts'
+import { organisationFactory } from '../../../../organisations/factories/organisation.factory.ts'
+import { pollFactory } from '../../../factories/poll.factory.ts'
+import { simulationPollFactory } from '../../../factories/simulation-poll.factory.ts'
+import { createComputePollStats } from '../compute-poll-stats.ts'
+
+const logger = {
+  error: vi.fn(),
+  warn: vi.fn(),
+  info: vi.fn(),
+  debug: vi.fn(),
+}
+
+const computePollStats = createComputePollStats({ logger })
+
+describe('computePollStats', () => {
+  afterEach(async () => {
+    await prisma.simulationPoll.deleteMany()
+    await prisma.poll.deleteMany()
+    await prisma.simulation.deleteMany()
+    await prisma.organisation.deleteMany()
+  })
+
+  it('sums computedResults across valid simulations and ignores invalid ones', async () => {
+    const organisation = await organisationFactory.create()
+    const poll = await pollFactory.create({ organisationId: organisation.id })
+
+    const validSimulation1 = await simulationPollFactory
+      .completed()
+      .withPollId(poll.id)
+      .create()
+    const validSimulation2 = await simulationPollFactory
+      .completed()
+      .withPollId(poll.id)
+      .create()
+    await simulationPollFactory.started().withPollId(poll.id).create()
+
+    const { computedResults } = await computePollStats(poll.id)
+
+    expect(computedResults.carbone.bilan).toBe(
+      validSimulation1.computedResults.carbone.bilan +
+        validSimulation2.computedResults.carbone.bilan
+    )
+  })
+
+  it('derives fun facts from the situation', async () => {
+    const organisation = await organisationFactory.create()
+    const poll = await pollFactory.create({ organisationId: organisation.id })
+
+    await simulationPollFactory
+      .completed()
+      .params({
+        situation: {
+          'transport . voiture . km': 12000,
+          'transport . mobilité douce . vélo . présent': 'oui',
+        },
+      })
+      .withPollId(poll.id)
+      .create()
+
+    const { funFacts } = await computePollStats(poll.id)
+
+    expect(funFacts.averageOfCarKilometers).toBe(12000)
+    expect(funFacts.percentageOfBicycleUsers).toBe(100)
+  })
+})

packages/core/src/features/polls/stats/repositories/poll-stats.repository.ts
@@ -1,0 +1,18 @@
+import type { FunFacts } from '@incubateur-ademe/nosgestesclimat'
+import { prisma } from '../../../../prisma/client.ts'
+import type { ComputedResultSchema } from '../../../simulations/validators/computed-results.schema.ts'
+
+export const countPollSimulations = (pollId: string) =>
+  prisma.simulationPoll.count({ where: { pollId } })
+
+export const updatePollStats = (
+  pollId: string,
+  {
+    computedResults,
+    funFacts,
+  }: { computedResults: ComputedResultSchema; funFacts: FunFacts }
+) =>
+  prisma.poll.update({
+    where: { id: pollId },
+    data: { computedResults, funFacts },
+  })

packages/core/src/features/polls/stats/services/process-next-pending-poll-stats.ts
@@ -1,0 +1,76 @@
+import type { FunFacts } from '@incubateur-ademe/nosgestesclimat'
+import type { ComputedResultSchema } from '../../../simulations/validators/computed-results.schema.ts'
+import { PollStatsComputationFailedException } from '../exceptions/poll-stats-computation.exception.ts'
+import { resolvePollStatsCooldownSeconds } from '../helpers/policy.ts'
+import {
+  claimNextPendingPollStatsComputation,
+  markPollStatsComputationCompleted,
+  markPollStatsComputationFailed,
+  releasePollStatsComputation,
+} from '../repositories/poll-stats-computations.repository.ts'
+import {
+  countPollSimulations,
+  updatePollStats,
+} from '../repositories/poll-stats.repository.ts'
+
+type ComputePollStats = (pollId: string) => Promise<{
+  computedResults: ComputedResultSchema
+  funFacts: FunFacts
+}>
+
+/**
+ * Entry point called by the worker in a loop.
+ *
+ * Claims the next eligible PollStatsComputation job, computes the poll's
+ * participation count to resolve its cooldown, defers the job (via `runAt`)
+ * when the cooldown has not elapsed yet, otherwise recomputes the poll stats
+ * and stores them on the poll. Returns `true` when a recompute was completed,
+ * `false` when the queue was empty or the job was deferred.
+ */
+export function createProcessNextPendingPollStats({
+  computePollStats,
+}: {
+  computePollStats: ComputePollStats
+}) {
+  return async function processNextPendingPollStats(): Promise<boolean> {
+    const claimed = await claimNextPendingPollStatsComputation()
+    if (!claimed) {
+      return false
+    }
+
+    const { pollId, completedAt } = claimed
+
+    try {
+      const count = await countPollSimulations(pollId)
+      const cooldownSeconds = resolvePollStatsCooldownSeconds(count)
+
+      if (completedAt) {
+        const nextRunAt = completedAt.getTime() + cooldownSeconds * 1000
+        if (nextRunAt > Date.now()) {
+          await releasePollStatsComputation(pollId, new Date(nextRunAt))
+          return false
+        }
+      }
+
+      const { computedResults, funFacts } = await computePollStats(pollId)
+
+      await updatePollStats(pollId, { computedResults, funFacts })
+
+      await markPollStatsComputationCompleted(pollId)
+      return true
+    } catch (error) {
+      try {
+        await markPollStatsComputationFailed(pollId)
+      } catch (cleanupError) {
+        throw new PollStatsComputationFailedException({
+          pollId,
+          cause: new SuppressedError(cleanupError, error),
+        })
+      }
+      throw new PollStatsComputationFailedException({
+        pollId,
+        cause: error,
+      })
+    }
+  }
+}

incubateur-ademe/nosgestesclimat-app

packages/core/src/features/polls/stats/legacy/__tests__/compute-poll-stats.spec.ts
@@ -1,0 +1,67 @@
+import { afterEach, describe, expect, it, vi } from 'vitest'
+import { prisma } from '../../../../../prisma/client.ts'
+import { organisationFactory } from '../../../../organisations/factories/organisation.factory.ts'
+import { simulationFactory } from '../../../../simulations/factories/simulation.factory.ts'
+import { pollFactory } from '../../../factories/poll.factory.ts'
+import { createComputePollStats } from '../compute-poll-stats.ts'
+
+const logger = {
+  error: vi.fn(),
+  warn: vi.fn(),
+  info: vi.fn(),
+  debug: vi.fn(),
+}
+
+const computePollStats = createComputePollStats({ logger })
+
+describe('computePollStats', () => {
+  afterEach(async () => {
+    await prisma.simulationPoll.deleteMany()
+    await prisma.poll.deleteMany()
+    await prisma.simulation.deleteMany()
+    await prisma.organisation.deleteMany()
+  })
+
+  it('sums computedResults across valid simulations and ignores invalid ones', async () => {
+    const organisation = await organisationFactory.create()
+    const poll = await pollFactory.withOrganisation(organisation.id).create()
+
+    const validSimulation1 = await simulationFactory
+      .completed()
+      .withPollId(poll.id)
+      .create()
+    const validSimulation2 = await simulationFactory
+      .completed()
+      .withPollId(poll.id)
+      .create()
+    await simulationFactory.started().withPollId(poll.id).create()
+
+    const { computedResults } = await computePollStats(poll.id)
+
+    expect(computedResults.carbone.bilan).toBe(
+      validSimulation1.computedResults.carbone.bilan +
+        validSimulation2.computedResults.carbone.bilan
+    )
+  })
+
+  it('derives fun facts from the situation', async () => {
+    const organisation = await organisationFactory.create()
+    const poll = await pollFactory.withOrganisation(organisation.id).create()
+
+    await simulationFactory
+      .completed()
+      .params({
+        situation: {
+          'transport . voiture . km': 12000,
+          'transport . mobilité douce . vélo . présent': 'oui',
+        },
+      })
+      .withPollId(poll.id)
+      .create()
+
+    const { funFacts } = await computePollStats(poll.id)
+
+    expect(funFacts.averageOfCarKilometers).toBe(12000)
+    expect(funFacts.percentageOfBicycleUsers).toBe(100)
+  })
+})

packages/core/src/features/polls/stats/repositories/poll-stats.repository.ts
@@ -1,0 +1,18 @@
+import type { FunFacts } from '@incubateur-ademe/nosgestesclimat'
+import { prisma } from '../../../../prisma/client.ts'
+import type { ComputedResults } from '../../../simulations/validators/computed-results.schema.ts'
+
+export const countPollSimulations = (pollId: string) =>
+  prisma.simulationPoll.count({ where: { pollId } })
+
+export const updatePollStats = (
+  pollId: string,
+  {
+    computedResults,
+    funFacts,
+  }: { computedResults: ComputedResults; funFacts: FunFacts }
+) =>
+  prisma.poll.update({
+    where: { id: pollId },
+    data: { computedResults, funFacts },
+  })

packages/core/src/features/polls/stats/services/process-next-pending-poll-stats.ts
@@ -1,0 +1,76 @@
+import type { FunFacts } from '@incubateur-ademe/nosgestesclimat'
+import type { ComputedResults } from '../../../simulations/validators/computed-results.schema.ts'
+import { PollStatsComputationFailedException } from '../exceptions/poll-stats-computation.exception.ts'
+import { resolvePollStatsCooldownSeconds } from '../helpers/policy.ts'
+import {
+  claimNextPendingPollStatsComputation,
+  markPollStatsComputationCompleted,
+  markPollStatsComputationFailed,
+  releasePollStatsComputation,
+} from '../repositories/poll-stats-computations.repository.ts'
+import {
+  countPollSimulations,
+  updatePollStats,
+} from '../repositories/poll-stats.repository.ts'
+
+type ComputePollStats = (pollId: string) => Promise<{
+  computedResults: ComputedResults
+  funFacts: FunFacts
+}>
+
+/**
+ * Entry point called by the worker in a loop.
+ *
+ * Claims the next eligible PollStatsComputation job, computes the poll's
+ * participation count to resolve its cooldown, defers the job (via `runAt`)
+ * when the cooldown has not elapsed yet, otherwise recomputes the poll stats
+ * and stores them on the poll. Returns `true` when a recompute was completed,
+ * `false` when the queue was empty or the job was deferred.
+ */
+export function createProcessNextPendingPollStats({
+  computePollStats,
+}: {
+  computePollStats: ComputePollStats
+}) {
+  return async function processNextPendingPollStats(): Promise<boolean> {
+    const claimed = await claimNextPendingPollStatsComputation()
+    if (!claimed) {
+      return false
+    }
+
+    const { pollId, completedAt } = claimed
+
+    try {
+      const count = await countPollSimulations(pollId)
+      const cooldownSeconds = resolvePollStatsCooldownSeconds(count)
+
+      if (completedAt) {
+        const nextRunAt = completedAt.getTime() + cooldownSeconds * 1000
+        if (nextRunAt > Date.now()) {
+          await releasePollStatsComputation(pollId, new Date(nextRunAt))
+          return false
+        }
+      }
+
+      const { computedResults, funFacts } = await computePollStats(pollId)
+
+      await updatePollStats(pollId, { computedResults, funFacts })
+
+      await markPollStatsComputationCompleted(pollId)
+      return true
+    } catch (error) {
+      try {
+        await markPollStatsComputationFailed(pollId)
+      } catch (cleanupError) {
+        throw new PollStatsComputationFailedException({
+          pollId,
+          cause: new SuppressedError(cleanupError, error),
+        })
+      }
+      throw new PollStatsComputationFailedException({
+        pollId,
+        cause: error,
+      })
+    }
+  }
+}