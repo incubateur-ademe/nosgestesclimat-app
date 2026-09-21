e1b3f816272155f3e88dbbfbb5b7e80a91df6120
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2026-09-21 14:57:50 +0200

    Revert "clean after rebasing"
    This reverts commit 877995059c6b04d97ec4ca64e291d42126d517a1.

--- apps/site/src/components/translation/LanguageSwitchButton.tsx
+++ apps/site/src/components/translation/LanguageSwitchButton.tsx
@@ -69,6 +69,11 @@ export default function LanguageSwitchButton({ className }: Props) {
                 completeLanguageString: inactiveLang.completeLanguageString,
               }
             )}
+            onClick={() => {
+              trackPosthogEvent(
+                captureClickLanguage({ locale: inactiveLocale })
+              )
+            }}
             className="hover:bg-primary-50 active:bg-primary-100 rounded-sm px-2 py-2">
             <Emoji className="mr-2">{inactiveLang.flag}</Emoji>
             <span className="text-primary-700 text-base font-normal capitalize">
