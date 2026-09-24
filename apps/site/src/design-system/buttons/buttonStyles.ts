4bf4d46148b4ecf48cb5c8ef192a60c7faf56e83
Benjamin Arias <12382534+bjlaa@users.noreply.github.com>
2026-09-24 12:20:20 +0200

    Update buttonStyles.ts

--- apps/site/src/design-system/buttons/buttonStyles.ts
+++ apps/site/src/design-system/buttons/buttonStyles.ts
@@ -42,4 +42,4 @@ export const loaderColorMap: Record<ButtonColor, 'light' | 'dark'> = {
 }
 
 export const baseClassNames =
-  'active:scale-95 duration-100! transition-all transition- inline-flex items-center opacity-100! justify-center whitespace-nowrap rounded-full font-bold no-underline aria-disabled:opacity-50 leading-none!'
+  'duration-100! transition-all transition- inline-flex items-center opacity-100! justify-center whitespace-nowrap rounded-full font-bold no-underline aria-disabled:opacity-50 leading-none!'
